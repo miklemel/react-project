58e23ba94607138fa4ad250262b4233d7e1f2b67
mihailmalyhin <malykhinmikhail@rambler.ru>
2023-02-08 11:26:18 +0100

    initial

---
+++ config/build/buildLoaders.ts
@@ -0,0 +1,24 @@
+import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
+
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            options.isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+        ]
+    }
+    const typescriptLoaders = {
+        test: /\.tsx?$/,
+            use: 'ts-loader',
+        exclude: /node_modules/,
+    }
+
+    return [
+        typescriptLoaders,
+        cssLoader
+    ]
+}
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import {render} from "react-dom";
+import Counter from "./components/Counter";
+
+render(
+    <Counter></Counter>,
+    document.getElementById('root')
+)
