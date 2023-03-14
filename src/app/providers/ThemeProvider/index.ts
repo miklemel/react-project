75419f33ca40f6f1270a45159ddbd86bbcd9cf34
mihailmalyhin <malykhinmikhail@rambler.ru>
2023-03-14 14:15:24 +0100

    feat: all all all

---
+++ src/app/providers/ThemeProvider/index.ts
@@ -0,0 +1,9 @@
+import {useTheme} from "./lib/useTheme";
+import {Theme} from "./lib/ThemeContext";
+import ThemeProvider from "./ui/ThemeProvider";
+
+export {
+    useTheme,
+    Theme,
+    ThemeProvider
+}
