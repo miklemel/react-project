5a7021bc56c39de5050a7b430c747562868d5e12
mihailmalyhin <malykhinmikhail@rambler.ru>
2023-05-28 18:55:09 +0200

    refactor

--- src/pages/AboutPage/ui/AboutPage.async.tsx
+++ src/pages/AboutPage/ui/AboutPage.async.tsx
@@ -1,6 +1,7 @@
 import { lazy } from 'react';
 
 export const AboutPageAsync = lazy(() => new Promise((resolve) => {
-  // @ts-ignore
-  setTimeout(() => resolve(import('./AboutPage')), 1500);
+    // @ts-ignore
+    // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
+    setTimeout(() => resolve(import('./AboutPage')), 1500);
 }));
